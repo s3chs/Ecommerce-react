bc14632a9d22a7cd6da8a73c53f316001e5482da
sechs <philippe.buteau@skema.edu>
2021-07-09 16:53:29 +0200

    product showcase working, on to redux

--- src/Pages/ProductShowcase/ProductShowcase.js
+++ src/Pages/ProductShowcase/ProductShowcase.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import "./ProductShowcase.css";
+import inventory from "../../data/inventory";
+import { useDispatch } from "react-redux";
 
 export default function ProductShowcase() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [nbMugs, setNbMugs] = useState(1);
+
+  const { id } = useParams();
+  //   console.log(id);
+
+  const productClicked = inventory.findIndex(
+    (obj) => obj.title.replace(/\s+/g, "").trim() === id
+  );
+
+  //   console.log(productClicked);
+
+  const updateMugs = (e) => {
+    setNbMugs(Number(e.target.value));
+  };
+
+  return (
+    <div className="showcase">
+      <div className="container-img-showcase">
+        <img
+          className="img-showcase"
+          src={
+            process.env.PUBLIC_URL +
+            `/images/${inventory[productClicked].img}.png`
+          }
+          alt="product-pic"
+        />
+      </div>
+      <div className="product-infos">
+        <h2>{inventory[productClicked].title}</h2>
+        <p>Prix: {inventory[productClicked].price}€</p>
+        <form>
+          <label htmlFor="quantity">Quantité</label>
+          <input
+            type="number"
+            id="quantity"
+            value={nbMugs}
+            onChange={updateMugs}
+          />
+          <button>Ajouter au panier</button>
+          <span className="adding-info"></span>
+        </form>
+      </div>
+    </div>
+  );
 }
