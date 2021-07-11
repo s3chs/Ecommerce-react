2eb28d2d45f66ba10ab8113c6063672b96aea746
sechs <philippe.buteau@skema.edu>
2021-07-11 15:36:54 +0200

    project done

--- src/Components/FloatingCart/FloatingCart.js
+++ src/Components/FloatingCart/FloatingCart.js
@@ -2,15 +2,23 @@ import React from "react";
 import cartIcon from "./shopping-cart.svg";
 import "./FloatingCart.css";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function FloatingCart() {
+  // Permet de nous envoyer le state du shopping cart //
+  const shoppingCart = useSelector((state) => state);
+  // console.log(shoppingCart);
+  let totalItems = 0;
+  for (const item of shoppingCart.cart) {
+    totalItems += item.quantity;
+  }
   return (
-    <Link to="shoppingCart">
+    <Link to="/panier">
       <div className="floating-cart">
         <p>Votre Panier</p>
         <div className="img-notif-container">
           <img src={cartIcon} alt="icone panier" />
-          <span className="notif">0</span>
+          <span className="notif">{totalItems}</span>
         </div>
       </div>
     </Link>
--- src/redux/reducer/cartReducer.js
+++ src/redux/reducer/cartReducer.js
@@ -9,20 +9,39 @@ export default function cartReducer(state = INITIAL_STATE, action) {
         (obj) => obj.id === action.payload.id
       );
       if (indexItemAdd !== -1) {
+        const updatedQuantity = {
+          ...state.cart[indexItemAdd],
+          quantity: state.cart[indexItemAdd].quantity + action.payload.quantity,
+        };
+        const newArr = [...state.cart];
+        newArr.splice(indexItemAdd, 1, updatedQuantity);
+        // console.log(newArr);
+
+        return {
+          cart: newArr,
+        };
       } else {
         const newArr = [...state.cart];
         newArr.push(action.payload);
-        console.log(newArr);
+        // console.log(newArr);
         return {
           cart: newArr,
         };
       }
-    //   console.log(indexItemAdd); 
+    //   console.log(indexItemAdd);
 
     case "UPDATEITEM":
-      return {};
+      const indexItemUpdate = state.cart.findIndex(
+        (obj) => obj.id === action.payload.id
+      );
+      const newArr = [...state.cart];
+      newArr.splice(indexItemUpdate, 1, action.payload);
+      return {
+        cart: newArr,
+      };
 
     default:
-      console.log("default case");
+    //   console.log("default case");
   }
+  return state;
 }
