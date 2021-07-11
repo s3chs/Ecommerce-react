a08ccf5611cb2f27992023327dc077bb1fc1c8ed
sechs <philippe.buteau@skema.edu>
2021-07-11 13:40:54 +0200

    redux initialized

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 
-      <App />,
   document.getElementById("root")
 );
